81b335f05f987f17f1f6cbdc9590bceec94f911a
Yashvi Desaii <yashvi.desai999@gmail.com>
2024-03-30 15:50:22 -0700

    added login page

--- Macroeconomic-Researcher/macroeconomic-ui/src/App.jsx
+++ Macroeconomic-Researcher/macroeconomic-ui/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./components/login";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1> Hi</h1>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
