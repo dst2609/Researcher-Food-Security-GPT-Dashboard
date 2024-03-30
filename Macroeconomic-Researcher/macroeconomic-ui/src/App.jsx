8e03efaeccf68976bbf8720ab5b2a1c2cc481ec8
Devarsh Thaker <devarsht@gmail.com>
2024-03-30 16:43:51 -0700

    added chatUI

--- Macroeconomic-Researcher/macroeconomic-ui/src/App.jsx
+++ Macroeconomic-Researcher/macroeconomic-ui/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Login from "./components/login";
+import Login from "./Components/Login/login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatUI from "./Components/ChatUI/ChatUI";
 
---
+++ Macroeconomic-Researcher/macroeconomic-ui/src/components/Login/login.jsx
@@ -0,0 +1,50 @@
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./login.module.css";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    navigate("/home");
+  };
+
+  return (
+    <div className={styles.loginBg}>
+      <form>
+        <div className={styles.login}>
+          <header>
+            <h2>Sign In</h2>
+          </header>
+
+          <div className={styles.formElements}>
+            <label htmlFor="username">
+              <h4>Email_ID</h4>
+            </label>{" "}
+            &nbsp;
+            <input type="email" name="email"></input>
+          </div>
+
+          <div className={styles.formElements}>
+            <label htmlFor="password">
+              <h4>Password</h4>
+            </label>{" "}
+            &nbsp;
+            <input type="password" name="password"></input>
+          </div>
+
+          <div className={styles.formElements}>
+            <input
+              onClick={(e) => onLogin(e)}
+              className={styles.buttonLogin}
+              id="form-btn"
+              type="submit"
+              value="Login"
+            ></input>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default Login;
