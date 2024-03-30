6455d182c10e518995b0dd3be3cfcd2ce5e292c3
Devarsh Thaker <devarsht@gmail.com>
2024-03-30 14:48:25 -0700

    test commit

--- Hackathon/api/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const { handleGreeting, handleChat } = require("../controllers/controllers");
-
-module.exports = function (app) {
-  // GET route
-  app.get("/greeting", handleGreeting);
-
-  // POST route for chat requests
-  app.post("/chat", handleChat);
-};
