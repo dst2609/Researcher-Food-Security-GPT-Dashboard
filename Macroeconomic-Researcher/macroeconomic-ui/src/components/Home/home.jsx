042f45676bafbd1c1af5c8bf9ce2ee05ca1abd70
Yashvi Desaii <yashvi.desai999@gmail.com>
2024-03-30 17:24:21 -0700

    added home page

---
+++ Macroeconomic-Researcher/macroeconomic-ui/src/components/Home/home.jsx
@@ -0,0 +1,12 @@
+import Header from "../Header/header";
+
+
+export default function Home() {
+  return (
+    <div>
+      <div className="row">
+        <Header />
+      </div>
+    </div>
+  );
+}
